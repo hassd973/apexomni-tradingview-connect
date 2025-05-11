55c72af48fa7bc0259921895147fcbef7da6540b
hassd973 <78494014+hassd973@users.noreply.github.com>
2025-05-11 09:54:13 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const Logtail = require('@logtail/node');
+const { Node: Logtail } = require("@logtail/js");
 
 const app = express();
 const port = process.env.PORT || 3000;
